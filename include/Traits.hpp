fc4dbd21205ca435636847fb46625d012460332f
Y2J2142 <y2j2142@hotmail.com>
2020-08-03 00:14:19 +0200

    Add a bunch of stuff that might be usefull later

--- include/Traits.hpp
+++ include/Traits.hpp
@@ -1,4 +1,6 @@
 #pragma once
+#include <array>
+#include <cstddef>
 #include <type_traits>
 namespace Functional {
 
@@ -21,5 +23,45 @@ template <typename NewType, template <typename...> typename Base,
 struct SwapTemplateParameter<NewType, Base<OldType>> {
 	using Type = Base<NewType>;
 };
+
+template <typename...>
+struct IsStdArray : std::false_type {};
+
+template <typename T, std::size_t N>
+struct IsStdArray<std::array<T, N>> : std::true_type {};
+
+template <typename... T>
+constexpr inline bool IsStdArray_v = IsStdArray<T...>::value;
+
+template <typename T>
+concept Bool = std::is_convertible_v<T, bool> || std::is_same_v<T, bool>;
+
+template <typename T>
+concept TrueType = std::is_base_of_v<std::true_type, T>
+	&&std::is_convertible_v<T, std::true_type>;
+
+template <typename T>
+concept FalseType = std::is_base_of_v<std::false_type, T>
+	&&std::is_convertible_v<T, std::false_type>;
+
+template <Bool T>
+constexpr inline bool Not(T &&t) {
+	return !std::forward<T>(t);
+}
+
+template <typename T>
+concept BooleanType = TrueType<T> || FalseType<T>;
+
+template <typename T, typename = void>
+struct NotType {};
+
+template <TrueType T>
+struct NotType<T> : std::false_type {};
+
+template <FalseType T>
+struct NotType<T> : std::true_type {};
+
+template <BooleanType T>
+constexpr inline bool NotType_v = NotType<T>::value;
 
 } // namespace Functional
--- tests/Traits.cpp
+++ tests/Traits.cpp
@@ -12,3 +12,37 @@ TEST_CASE("SwapTemplate testing", "[trait]") {
 	using floatVector = SwapTemplateParameter<float, std::vector<int>>::Type;
 	REQUIRE(std::is_same_v<floatVector, std::vector<float>>);
 }
+
+TEST_CASE("IsStdArray testing", "[trait]") {
+	using floatArr = std::array<float, 1>;
+	auto isArr = IsStdArray<floatArr>::value;
+	REQUIRE(IsStdArray<floatArr>::value);
+	REQUIRE(IsStdArray<float>::value == false);
+	REQUIRE(IsStdArray_v<int[4]> == false);
+}
+
+TEST_CASE("Bool concept", "[trait]") {
+	REQUIRE(Bool<int>);
+	REQUIRE(Bool<std::vector<int>> == false);
+}
+
+TEST_CASE("BooleanType concept", "[trait]") {
+	REQUIRE(BooleanType<std::true_type>);
+	REQUIRE(BooleanType<std::false_type>);
+	struct Derived : std::true_type {};
+	REQUIRE(BooleanType<Derived>);
+	REQUIRE(BooleanType<char> == false);
+	REQUIRE(BooleanType<char> == false);
+}
+
+TEST_CASE("NotType struct", "[trait]") {
+	REQUIRE(NotType_v<std::false_type>);
+	REQUIRE(NotType_v<std::true_type> == false);
+	struct Derived : std::true_type {};
+	REQUIRE(NotType_v<Derived> == false);
+}
+
+TEST_CASE("Not function", "[trait]") {
+	REQUIRE(Not(false));
+	REQUIRE(Not(true) == false);
+}

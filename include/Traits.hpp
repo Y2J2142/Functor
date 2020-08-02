07cc9771757885c3221df3298bc8a7bb48b98693
Y2J2142 <y2j2142@hotmail.com>
2020-08-02 13:36:18 +0200

    Add SwapeTemplateParameter

--- include/Traits.hpp
+++ include/Traits.hpp
@@ -8,4 +8,18 @@ struct IsSpecialization : std::false_type {};
 template <template <typename...> class Ref, typename... Args>
 struct IsSpecialization<Ref<Args...>, Ref> : std::true_type {};
 
-} // namespace Functor
+template <typename...>
+struct SwapTemplateParameter {};
+
+template <typename NewType, template <typename...> typename Base,
+		  typename OldType, typename... Args>
+struct SwapTemplateParameter<NewType, Base<OldType, Args...>> {
+	using Type = Base<NewType, Args...>;
+};
+template <typename NewType, template <typename...> typename Base,
+		  typename OldType>
+struct SwapTemplateParameter<NewType, Base<OldType>> {
+	using Type = Base<NewType>;
+};
+
+} // namespace Functional
--- tests/Traits.cpp
+++ tests/Traits.cpp
@@ -1,9 +1,14 @@
 #include "Traits.hpp"
 #include "catch2/catch.hpp"
+#include <fmt/core.h>
 #include <vector>
 using namespace Functional;
 TEST_CASE("IsSpecialization testing", "[trait]") {
 	std::vector v{1, 2, 3};
 	REQUIRE(IsSpecialization<decltype(v), std::vector>::value);
 	REQUIRE(IsSpecialization<int, std::vector>::value == false);
+}
+TEST_CASE("SwapTemplate testing", "[trait]") {
+	using floatVector = SwapTemplateParameter<float, std::vector<int>>::Type;
+	REQUIRE(std::is_same_v<floatVector, std::vector<float>>);
 }

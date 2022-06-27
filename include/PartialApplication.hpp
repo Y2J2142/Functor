f4539f845d9f12962a33d7bbded9b35ffa1a44f5
Y2J2142 <Y2J2142@hotmail.com>
2022-06-27 11:54:04 +0200

    Switch from >= to <= for partial application

--- include/PartialApplication.hpp
+++ include/PartialApplication.hpp
@@ -4,7 +4,7 @@
 namespace Functional {
 
 template <class F, class Arg>
-constexpr auto operator>=(F &&f, Arg &&arg) {
+constexpr auto operator<=(F &&f, Arg &&arg) {
 	return std::bind_front(std::forward<F>(f), std::forward<Arg>(arg));
 }
 
--- tests/PartialApplication.cpp
+++ tests/PartialApplication.cpp
@@ -13,17 +13,23 @@ auto sub(int a, int b) { return a - b; }
 
 TEST_CASE("Partial Application with lambda", "[partial]") {
 	auto add = [](int x, int y) { return x + y; };
-	constexpr auto add10 = add >= 10;
+	constexpr auto add10 = add <= 10;
 
 	REQUIRE(add10(5) == add(10, 5));
 }
 
 TEST_CASE("Partial Application with functions", "[partial]") {
-	auto subtractFrom10 = _w(sub) >= 10;
+	auto subtractFrom10 = _w(sub) <= 10;
 	REQUIRE(subtractFrom10(5) == sub(10, 5));
 }
 
 TEST_CASE("Partial Application with std::function", "[partial]") {
-	auto sub10 = std::function{sub} >= 10;
+	auto sub10 = std::function{sub} <= 10;
 	REQUIRE(sub10(5) == sub(10, 5));
+}
+
+TEST_CASE("Chained Partial Application", "[partial]") {
+	auto fn = [](int a, int b, int c) { return a + b * c; };
+	auto f = fn <= 10 <= 5;
+	REQUIRE(f(2) == fn(10, 5, 2));
 }
